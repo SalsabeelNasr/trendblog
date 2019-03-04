a376f024eaeb7cf785f38dc34fc83e0af3cf616c
Salsabeel Khaleel <salsabeelnasr@gmail.com>
2019-03-04 22:58:43 +0100

    epic fail

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-
+import { createSwitchNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
+import AuthLoadingScreen from '../screens/AuthLoadingScreen';
+import SignInScreen from '../screens/SignInScreen';
 import MainTabNavigator from './MainTabNavigator';
 
-export default createAppContainer(createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
-}));
+// Implementation of HomeScreen, OtherScreen, SignInScreen, AuthLoadingScreen
+// goes here.
+
+const AppStack = MainTabNavigator;
+const AuthStack = createStackNavigator({ SignIn: SignInScreen });
+
+export default createAppContainer(createSwitchNavigator(
+  {
+    AuthLoading: AuthLoadingScreen,
+    App: AppStack,
+    Auth: AuthStack,
+  },
+  {
+    initialRouteName: 'App',
+  }
+));
